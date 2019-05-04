0030107452f4a03c7c2f13952ead0b864531d53a
Yang <pourplusquoi@icloud.com>
2019-05-04 01:59:21 -0700

    Update eq.h

--- src/eq.h
+++ src/eq.h
@@ -3,7 +3,7 @@ struct Eq {};
 
 template <class Derived>
 bool operator==(const Eq<Derived>& lhs, const Eq<Derived>& rhs) {
-    const Derived& dlhs = static_cast<Derived const&>(lhs);
-    const Derived& drhs = static_cast<Derived const&>(rhs); 
-    return !dlhs.operator<(drhs) && !drhs.operator<(dlhs);
+    decltype(auto) dl = static_cast<const Derived&>(lhs);
+    decltype(auto) dr = static_cast<const Derived&>(rhs); 
+    return !dl.operator<(dr) && !dr.operator<(dl);
 }
