90f28912bf0e6238dada489ac9197ff6ab4256ef
Yang <pourplusquoi@icloud.com>
2019-05-04 16:03:54 -0700

    Update compare.h

--- src/compare.h
+++ src/compare.h
@@ -1,33 +1,57 @@
 #ifndef COMPARE_H_
 #define COMPARE_H_
 
-template <typename Derived>
-struct Compare {};
+template <typename T>
+struct Eq {};
 
-template <class Derived>
-bool operator==(const Compare<Derived>& lhs, const Compare<Derived>& rhs) {
-    decltype(auto) dl = static_cast<const Derived&>(lhs);
-    decltype(auto) dr = static_cast<const Derived&>(rhs); 
-    return !dl.operator<(dr) && !dr.operator<(dl);
+template <class T>
+bool operator==(const Eq<T>& lhs, const Eq<T>& rhs) {
+    decltype(auto) dl = static_cast<const T&>(lhs);
+    decltype(auto) dr = static_cast<const T&>(rhs);
+    return !(dl < dr) && !(dr < dl);
 }
 
-template <class Derived>
-bool operator>(const Compare<Derived>& lhs, const Compare<Derived>& rhs) {
-    decltype(auto) dl = static_cast<const Derived&>(lhs);
-    decltype(auto) dr = static_cast<const Derived&>(rhs); 
-    return dr.operator<(dl);
+template <typename T>
+struct Ne {};
+
+template <class T>
+bool operator!=(const Ne<T>& lhs, const Ne<T>& rhs) {
+    decltype(auto) dl = static_cast<const T&>(lhs);
+    decltype(auto) dr = static_cast<const T&>(rhs);
+    return dl < dr || dr < dl;
 }
 
-template <class Derived>
-bool operator<=(const Compare<Derived>& lhs, const Compare<Derived>& rhs) {
-    decltype(auto) dl = static_cast<const Derived&>(lhs);
-    decltype(auto) dr = static_cast<const Derived&>(rhs); 
-    return dl.operator<(dr) || operator==(lhs, rhs);
+template <typename T>
+struct Gt {};
+
+template <class T>
+bool operator>(const Gt<T>& lhs, const Gt<T>& rhs) {
+    decltype(auto) dl = static_cast<const T&>(lhs);
+    decltype(auto) dr = static_cast<const T&>(rhs);
+    return dr < dl;
 }
 
-template <class Derived>
-bool operator>=(const Compare<Derived>& lhs, const Compare<Derived>& rhs) {
-    return operator>(lhs, rhs) || operator==(lhs, rhs);
+template <typename T>
+struct Le {};
+
+template <class T>
+bool operator<=(const Le<T>& lhs, const Le<T>& rhs) {
+    decltype(auto) dl = static_cast<const T&>(lhs);
+    decltype(auto) dr = static_cast<const T&>(rhs);
+    return !(dr < dl);
 }
+
+template <typename T>
+struct Ge {};
+
+template <class T>
+bool operator>=(const Ge<T>& lhs, const Ge<T>& rhs) {
+    decltype(auto) dl = static_cast<const T&>(lhs);
+    decltype(auto) dr = static_cast<const T&>(rhs);
+    return !(dl < dr);
+}
+
+template <typename T>
+struct Compare : Eq<T>, Ne<T>, Gt<T>, Le<T>, Ge<T> {};
 
 #endif // COMPARE_H_
